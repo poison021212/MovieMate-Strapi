ca91b0c375a90a2edbd17e08af9af25febc81ba8
poison021212 <1650216007@qq.com>
2026-03-14 10:24:47 +0800

    配置 CORS 允许前端访问

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,7 +4,14 @@ const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://movie-mate-8j9b6l5yk-poison021212s-projects.vercel.app', 'http://localhost:5173'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
